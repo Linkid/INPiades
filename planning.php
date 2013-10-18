c29f07d5305f4e550fe8633d5f43ef2358633897
François Magimel <francois.magimel@etu.enseeiht.fr>
2013-10-18 22:19:25 +0200

    Init du planning

---
+++ planning.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" manifest="cache.manifest">
+  <head>
+    <title>INPiades 2013 — planning</title>
+    <?php include('head_.php'); ?>
+  </head>
+
+  <body>
+  <div data-role="page">
+    <?php include('core_body_.php'); ?>
+
+    <section data-role="collapsible-set">
+      <h1 data-role="collapsible" data-content-theme="d">9H - Accueil</h1>
+      <article>
+        Coucou !
+      </article>
+    </section>
+  </div>
+  </body>
+</html>
