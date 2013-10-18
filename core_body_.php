b5e63c74cde45801b4edd5cecb023cf57905b2d1
François Magimel <francois.magimel@etu.enseeiht.fr>
2013-10-18 21:36:05 +0200

    Entête HTML et core du body HTML (ce qui sera partout)

---
+++ core_body_.php
@@ -0,0 +1,10 @@
+<!-- Header -->
+<header data-role="header" data-theme="f">
+  <strong>INPiades 2013</strong>
+  <!-- Du 15 au 17 novembre 2013 -->
+</header>
+
+<!-- Footer -->
+<footer data-role="footer" role="contentinfo">
+  Dernière mise à jour :
+</footer>
---
+++ head_.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta name="apple-touch-fullscreen" content="yes" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="format-detection" content="telephone=no">
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<meta name="HandheldFriendly" content="true">
+
+<link rel="apple-touch-icon" href="style/apple-touch-icon.png" />
+<link rel="apple-touch-icon-precomposed" href="style/apple-touch-icon.png" />
+
+<!-- JQuery Mobile -->
+<link rel="stylesheet" type="text/css" href="css/jquery.mobile-1.3.2.min.css" media="screen, mobile" />
+<script src="js/jquery.-1.10.2.min.js"></script>
+<script src="js/jquery.mobile-1.3.2.min.js"></script>
